8103ff36a0011b652ce6f0a8b511abf5a34d659e
JalenB43 <160173213+JalenB43@users.noreply.github.com>
2024-02-15 14:36:30 -0500

    Add files via upload

---
+++ P1LAB1_BowensJalen.py
@@ -0,0 +1,17 @@
+ # Jalen Bowens
+ # 2/15/24
+ # P1LAB1
+ # This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+
+
+# Get the first name of user and give it to variable
+
+fname=input("Enter your first name: ")
+##print(fname) 
+# Get the first name of user and give it to variable
+
+lname=input("Enter your last name: ")
+#print(lname)
+#print out name and a welcome message
+
+print("Hello",fname,lname+'! Welcome to CTI-110')
