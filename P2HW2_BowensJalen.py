4b5642da6c9c96d5e1079eff17f3ce6dc3c39d1a
JalenB43 <160173213+JalenB43@users.noreply.github.com>
2024-03-05 14:33:26 -0500

    Add files via upload

---
+++ P2HW2_BowensJalen.py
@@ -0,0 +1,28 @@
+ #Jalen Bowens
+
+ #3/5/2024
+
+ #P2HW2
+
+ #Assignment assess student understanding of Lists
+
+##Module_1=float(input
+##my_list.append(Module_1)
+##Module_2=
+##my_list.append(Module_2)
+##Module_3=
+##my_list.append(Module_3)
+##Module_4=
+##my_list.append(Module_4)
+##Module_5=
+##my_list.append(Module_5)
+##Module_6=
+##my_list.append(Module_6)
+
+lowest=min(my_list)
+print(lowest)
+highest=max(my_list)
+print(highest)
+length=len(my_list)
+sum1=sum(my_list)
+ave=sum1/length
