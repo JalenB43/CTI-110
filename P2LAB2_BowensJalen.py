40a74578ac5cfbfcf052123ee91bf827921806f9
JalenB43 <160173213+JalenB43@users.noreply.github.com>
2024-03-05 10:04:03 -0500

    Add files via upload

---
+++ P2LAB2_BowensJalen.py
@@ -0,0 +1,23 @@
+# Jalen Bowens
+ # 2/29
+ # P2LAB2 
+ # tests student's knowledge of how to write code that uses a dictionary to store user input and displays output to the user
+ 
+
+automobiles = {"Camaro":18.21,
+               "Prius":52.36,
+               "Model S":110,
+               "Silverado":26}
+keys=automobiles.keys()
+values=automobiles.values()
+print("The keys in the dictionary are:")
+print(automobiles, sep = "keys")
+automobile=input('Enter a vehicle to see its mpg: ')
+print("The",automobile,"gets",values,"mpg.")
+#print (value)
+#user_num3 = int(input('Approximate amount of money for accomodations: '))
+##dict.fromkeys(automobiles.keys)
+
+
+
+
