6e37b05509c0099a3c6e95f01cdc30b85d5fef05
JalenB43 <160173213+JalenB43@users.noreply.github.com>
2024-03-07 14:04:34 -0500

    Add files via upload

--- P2LAB2_BowensJalen.py
+++ P2LAB2_BowensJalen.py
@@ -4,20 +4,26 @@
  # tests student's knowledge of how to write code that uses a dictionary to store user input and displays output to the user
  
 
-automobiles = {"Camaro":18.21,
-               "Prius":52.36,
-               "Model S":110,
-               "Silverado":26}
-keys=automobiles.keys()
-values=automobiles.values()
+automobiles_dict = {
+    "Camaro": 18.21,
+    "Prius": 52.36,
+    "Model S": 110,
+    "Silverado": 26
+    }
+automobiles_list = automobiles_dict
 print("The keys in the dictionary are:")
-print(automobiles, sep = "keys")
+print(*automobiles_list, sep=", ")
+print()
 automobile=input('Enter a vehicle to see its mpg: ')
-print("The",automobile,"gets",values,"mpg.")
-#print (value)
-#user_num3 = int(input('Approximate amount of money for accomodations: '))
-##dict.fromkeys(automobiles.keys)
-
+print()
+mpg=automobiles_dict.get(
+    automobile)
+print("The",automobile,"gets",mpg,"mpg.")
+print()
+miles = float(input("How many miles  will drive the " + automobile + "? "))
+print()
+gallons=miles/mpg
+print(f"{gallons:.2f} gallon(s) of gas are needed to drive the {automobile} {miles:.1f} miles.")
 
 
 
