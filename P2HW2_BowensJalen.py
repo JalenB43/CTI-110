a928605d62bc3d244dd6adbb45bfc27873085a98
JalenB43 <160173213+JalenB43@users.noreply.github.com>
2024-03-07 14:04:53 -0500

    Add files via upload

--- P2HW2_BowensJalen.py
+++ P2HW2_BowensJalen.py
@@ -5,24 +5,28 @@
  #P2HW2
 
  #Assignment assess student understanding of Lists
-
-##Module_1=float(input
-##my_list.append(Module_1)
-##Module_2=
-##my_list.append(Module_2)
-##Module_3=
-##my_list.append(Module_3)
-##Module_4=
-##my_list.append(Module_4)
-##Module_5=
-##my_list.append(Module_5)
-##Module_6=
-##my_list.append(Module_6)
-
+my_list=[]
+Module_1=float(input("Enter Module 1 Test Score: "))
+my_list.append(Module_1)
+Module_2=float(input("Enter Module 2 Test Score: "))
+my_list.append(Module_2)
+Module_3=float(input("Enter Module 3 Test Score: "))
+my_list.append(Module_3)
+Module_4=float(input("Enter Module 4 Test Score: "))
+my_list.append(Module_4)
+Module_5=float(input("Enter Module 5 Test Score: "))
+my_list.append(Module_5)
+Module_6=float(input("Enter Module 6 Test Score: "))
+my_list.append(Module_6)
+print("")
+print("------------Results------------")
 lowest=min(my_list)
-print(lowest)
+print(f"Lowest Grade:     {lowest:.2f}")
 highest=max(my_list)
-print(highest)
+print(f"Highest Grade:     {highest:.2f}")
 length=len(my_list)
 sum1=sum(my_list)
+print(f"Sum of Grades:     {sum1:.2f}")
 ave=sum1/length
+print(f"Average:     {ave:.2f}")
+print("---------------------------------------")
